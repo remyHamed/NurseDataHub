15364519ae0a35e657296bd272c6ad082aba3884
hemarydem <hamedremy@hotmail.fr>
2024-03-16 19:22:06 +0100

    init spring boot project

---
+++ src/main/java/NurseDataHub/NurseDataHub/NurseDataHubApplication.java
@@ -0,0 +1,13 @@
+package NurseDataHub.NurseDataHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseDataHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseDataHubApplication.class, args);
+	}
+
+}
